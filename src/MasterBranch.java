7439bb76761b1fcad5bc0d029dbd69418a8282f1
Dash <raulm8493@gmail.com>
2013-06-01 04:58:32 +0530

    finished for 1st release

---
+++ src/MasterBranch.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Rahul Manghnani
+ *
+ */
+public class MasterBranch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am the master branch!");
+	}
+
+}
